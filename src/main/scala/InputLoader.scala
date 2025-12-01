5d6970b045115f0e67d824bcfe82b68daf9bdbba
Alexandre Emion <alexandre.emion@gmail.com>
2025-12-01 14:01:21 +0100

    feat: day 1

--- src/main/scala/InputLoader.scala
+++ src/main/scala/InputLoader.scala
@@ -2,8 +2,8 @@ import scala.io.Source
 
 object InputLoader {
   def load(day: Int, test: Boolean): List[String] = {
-    val fileName = if (test) s"day${day}_test.txt" else s"day${day}.txt"
-    val source = Source.fromFile(s"resources/inputs/$fileName")
+    val fileName = if (test) s"test_inputs/day${day}.txt" else s"inputs/day${day}.txt"
+    val source = Source.fromFile(s"resources/$fileName")
     try source.getLines().toList
     finally source.close()
   }
--- src/main/scala/solvers/Day01Solver.scala
+++ src/main/scala/solvers/Day01Solver.scala
@@ -2,18 +2,46 @@
 
 object Day01Solver extends DaySolver {
   override def solveFirstStar(input: List[String]): String = {
-    "test1"
+    val start = 50
+    val instructions = input.map(split)
+    val (numberOfZeros, previous) = instructions.foldLeft((0, start)) {
+      case ((accNumberOfZeros, acc), instruction) =>  
+        val nextNumber = ((acc + instruction) % 100 + 100) % 100
+
+        val nextNumberOfZeros = accNumberOfZeros + (if (nextNumber == 0) 1 else 0)
+        (nextNumberOfZeros, nextNumber)
+    }
+    
+    numberOfZeros.toString
   }
 
+
+  def split(instruction: String): Int = 
+    instruction match
+      case s"L${value}" => -value.toInt
+      case s"R${value}" => value.toInt
+
   override def solveSecondStar(input: List[String]): String = {
-    "test1"
+    val start = 50
+    val instructions = input.map(split)
+    val (numberOfZeros, previous) = instructions.foldLeft((0, start)) {
+      case ((accNumberOfZeros, acc), instruction) =>  
+        val nextWithoutModulo = (acc + instruction)
+        val toAdd = Math.floorDiv(Math.abs(nextWithoutModulo), 100) + (if (nextWithoutModulo <= 0 && acc != 0) 1 else 0)
+        val nextNumber = ((nextWithoutModulo) % 100 + 100) % 100
+
+        val nextNumberOfZeros = accNumberOfZeros + toAdd 
+        (nextNumberOfZeros, nextNumber)
+    }
+
+    numberOfZeros.toString
   }
 
   override def expectedFirstStar(): String = {
-    "test1"
+    "3"
   }
 
   override def expectedSecondStar(): String = {
-    "test1"
+    "6"
   }
 }
